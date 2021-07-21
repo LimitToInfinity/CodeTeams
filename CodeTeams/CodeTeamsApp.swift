4a540ece988c6d8fc9bdb1889a20f03b05523fff
LimitToInfinity <21stLimitless@gmail.com>
2021-07-21 13:20:00 -0600

    Initial Commit

---
+++ CodeTeams/CodeTeamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeTeamsApp.swift
+//  CodeTeams
+//
+//  Created by Ahmed Gaber on 7/21/21.
+//
+
+import SwiftUI
+
+@main
+struct CodeTeamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
